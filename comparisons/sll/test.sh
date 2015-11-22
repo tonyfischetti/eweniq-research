c80a415d38d2e134acc61e9447c5e50b53576aa7
Tony Fischetti <tony.fischetti@gmail.com>
2015-11-22 13:59:38 -0500

    fixed bug not allocating +1 for NUL

--- comparisons/sll/test.sh
+++ comparisons/sll/test.sh
@@ -1,4 +1,6 @@
-< ~/Desktop/morewords ./filter > /dev/null
+< ~/Desktop/morewords ./eweniq-glib > /dev/null
+# < ~/Desktop/morewords ./eweniq > /dev/null
+
 # < ~/Desktop/morewords sort  > /dev/null
 #< ~/Desktop/morewords egrep '*'  > /dev/null
 
